4bcbe138e3349aee3c610f77bcaa61f8b864900d
Levent Suncar <80010942+leventsuncar@users.noreply.github.com>
2021-07-03 16:07:59 +0300

    Proje oluşturma.

---
+++ src/main/java/com/demo2/DemoProject2BackendApplication.java
@@ -0,0 +1,13 @@
+package com.demo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProject2BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoProject2BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo2/DemoProject2BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProject2BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
